3c51a076ab4643237a52f5c84a2354c3de9e1670
MrJuju0319 <16257583+MrJuju0319@users.noreply.github.com>
2026-05-10 14:25:26 +0200

    Build Jeedom Optimizer multi-tab configuration interface

--- desktop/js/optimizer.js
+++ desktop/js/optimizer.js
@@ -1,9 +1,48 @@
 "use strict";
 
 (function () {
-  if (typeof jeedom === 'undefined') {
-    return;
+  function selectCmd($input) {
+    if (typeof jeedom === 'undefined' || typeof jeedom.cmd === 'undefined') {
+      return;
+    }
+    jeedom.cmd.getSelectModal({cmd: {type: $input.data('subtype') || 'info'}}, function (result) {
+      if (result && result.human) {
+        $input.val(result.human).trigger('change');
+      }
+    });
   }
 
-  console.debug('[Optimizer] Interface desktop chargée');
+  $('body').off('click', '.bt_selectCmd').on('click', '.bt_selectCmd', function () {
+    var $input = $(this).closest('.input-group').find('.cmdSelector');
+    selectCmd($input);
+  });
+
+  $('#bt_saveGlobal').off('click').on('click', function () {
+    var payload = {};
+    $('.configKey').each(function () {
+      payload[$(this).attr('data-l1key')] = $(this).val();
+    });
+
+    jeedom.config.save({
+      plugin: 'optimizer',
+      configuration: payload,
+      error: function (error) {
+        $('#div_alert').showAlert({message: error.message, level: 'danger'});
+      },
+      success: function () {
+        $('#div_alert').showAlert({message: 'Configuration sauvegardée', level: 'success'});
+        $('#opt_mode').text(payload.global_mode);
+        $('#opt_comfort').text(payload.target_comfort + '°C');
+      }
+    });
+  });
+
+  jeedom.config.load({
+    plugin: 'optimizer',
+    configuration: ['global_mode', 'target_comfort'],
+    success: function (data) {
+      if (data.global_mode) $('[data-l1key="global_mode"]').val(data.global_mode);
+      if (data.target_comfort) $('[data-l1key="target_comfort"]').val(data.target_comfort);
+    }
+  });
 })();
--- desktop/php/optimizer.php
+++ desktop/php/optimizer.php
@@ -1,18 +1,85 @@
 <?php
-
 if (!isConnect('admin')) {
     throw new Exception(__('401 - Accès non autorisé', __FILE__));
 }
 
 sendVarToJS('eqType', 'optimizer');
-$eqLogics = eqLogic::byType('optimizer');
+
+$globalMode = config::byKey('global_mode', 'optimizer', 'auto');
+$targetComfort = config::byKey('target_comfort', 'optimizer', 21);
+$nextActions = config::byKey('next_actions', 'optimizer', '[]');
 ?>
 
-<div class="row row-overflow">
+<div class="row row-overflow" id="optimizerContainer">
   <div class="col-lg-12">
-    <legend><i class="fas fa-sliders-h"></i> {{Optimizer}}</legend>
-    <div class="alert alert-info">
-      {{Base du plugin Optimizer initialisée. Les fonctionnalités de pilotage intelligent seront ajoutées progressivement.}}
+    <legend><i class="fas fa-brain"></i> {{Optimizer}}</legend>
+
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#opt_dashboard" aria-controls="opt_dashboard" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Dashboard}}</a></li>
+      <li role="presentation"><a href="#opt_global" aria-controls="opt_global" role="tab" data-toggle="tab"><i class="fas fa-cogs"></i> {{Configuration globale}}</a></li>
+      <li role="presentation"><a href="#opt_zone" aria-controls="opt_zone" role="tab" data-toggle="tab"><i class="fas fa-map-marked-alt"></i> {{Configuration par zone}}</a></li>
+      <li role="presentation"><a href="#opt_rules" aria-controls="opt_rules" role="tab" data-toggle="tab"><i class="fas fa-sliders-h"></i> {{Règles de régulation}}</a></li>
+      <li role="presentation"><a href="#opt_learning" aria-controls="opt_learning" role="tab" data-toggle="tab"><i class="fas fa-graduation-cap"></i> {{Apprentissage}}</a></li>
+      <li role="presentation"><a href="#opt_history" aria-controls="opt_history" role="tab" data-toggle="tab"><i class="fas fa-history"></i> {{Historique / décisions}}</a></li>
+      <li role="presentation"><a href="#opt_diagnostic" aria-controls="opt_diagnostic" role="tab" data-toggle="tab"><i class="fas fa-stethoscope"></i> {{Diagnostic}}</a></li>
+    </ul>
+
+    <div class="tab-content" style="margin-top:12px;">
+      <div role="tabpanel" class="tab-pane active" id="opt_dashboard">
+        <div class="row">
+          <div class="col-md-3"><div class="alert alert-info"><b>{{État global}}</b><br><span id="opt_state_global">{{OK}}</span></div></div>
+          <div class="col-md-3"><div class="alert alert-success"><b>{{Mode}}</b><br><span id="opt_mode"><?php echo $globalMode; ?></span></div></div>
+          <div class="col-md-3"><div class="alert alert-warning"><b>{{Confort cible}}</b><br><span id="opt_comfort"><?php echo $targetComfort; ?>°C</span></div></div>
+          <div class="col-md-3"><div class="alert alert-primary"><b>{{Prochaines actions}}</b><br><span id="opt_next_actions"><?php echo htmlspecialchars($nextActions); ?></span></div></div>
+        </div>
+      </div>
+
+      <div role="tabpanel" class="tab-pane" id="opt_global">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label class="col-sm-3 control-label">{{Mode global}}</label>
+            <div class="col-sm-4">
+              <select class="form-control configKey" data-l1key="global_mode">
+                <option value="auto">{{Auto}}</option>
+                <option value="eco">{{Eco}}</option>
+                <option value="confort">{{Confort}}</option>
+                <option value="absence">{{Absence}}</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-3 control-label">{{Confort cible (°C)}}</label>
+            <div class="col-sm-2"><input type="number" class="form-control configKey" data-l1key="target_comfort" min="15" max="28" step="0.5"></div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-3"><a class="btn btn-success" id="bt_saveGlobal"><i class="fas fa-save"></i> {{Sauvegarder}}</a></div>
+          </div>
+        </form>
+      </div>
+
+      <div role="tabpanel" class="tab-pane" id="opt_zone">
+        <table class="table table-condensed" id="tableZones">
+          <thead><tr><th>{{Zone}}</th><th>{{Capteur température (Info)}}</th><th>{{Action chauffage}}</th><th>{{Action clim}}</th><th>{{Action volet}}</th><th>{{Action lumière}}</th><th>{{Action ventilation}}</th></tr></thead>
+          <tbody>
+            <tr>
+              <td><input class="form-control" value="Salon"></td>
+              <td><div class="input-group"><input class="form-control cmdSelector" data-subtype="info"><span class="input-group-btn"><a class="btn btn-default bt_selectCmd"><i class="fas fa-list"></i></a></span></div></td>
+              <td><div class="input-group"><input class="form-control cmdSelector" data-subtype="action"><span class="input-group-btn"><a class="btn btn-default bt_selectCmd"><i class="fas fa-list"></i></a></span></div></td>
+              <td><div class="input-group"><input class="form-control cmdSelector" data-subtype="action"><span class="input-group-btn"><a class="btn btn-default bt_selectCmd"><i class="fas fa-list"></i></a></span></div></td>
+              <td><div class="input-group"><input class="form-control cmdSelector" data-subtype="action"><span class="input-group-btn"><a class="btn btn-default bt_selectCmd"><i class="fas fa-list"></i></a></span></div></td>
+              <td><div class="input-group"><input class="form-control cmdSelector" data-subtype="action"><span class="input-group-btn"><a class="btn btn-default bt_selectCmd"><i class="fas fa-list"></i></a></span></div></td>
+              <td><div class="input-group"><input class="form-control cmdSelector" data-subtype="action"><span class="input-group-btn"><a class="btn btn-default bt_selectCmd"><i class="fas fa-list"></i></a></span></div></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div role="tabpanel" class="tab-pane" id="opt_rules"><div class="alert alert-info">{{Onglet règles de régulation (priorités, hystérésis, contraintes énergétiques).}}</div></div>
+      <div role="tabpanel" class="tab-pane" id="opt_learning"><div class="alert alert-info">{{Onglet apprentissage (profil d’occupation, inertie thermique, habitudes).}}</div></div>
+      <div role="tabpanel" class="tab-pane" id="opt_history"><div class="alert alert-info">{{Onglet historique/décisions (journal des arbitrages et actions envoyées).}}</div></div>
+      <div role="tabpanel" class="tab-pane" id="opt_diagnostic"><div class="alert alert-info">{{Onglet diagnostic (cohérence capteurs, commandes manquantes, latence).}}</div></div>
     </div>
   </div>
 </div>
+
+<?php include_file('desktop', 'optimizer', 'js', 'optimizer'); ?>
