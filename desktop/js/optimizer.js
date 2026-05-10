5912845e9f9c3fae21255afce0b44ad196e699d2
MrJuju0319 <16257583+MrJuju0319@users.noreply.github.com>
2026-05-10 18:13:49 +0200

    Sépare le mode chauffage en commandes chaud et froid

--- desktop/js/optimizer.js
+++ desktop/js/optimizer.js
@@ -173,7 +173,8 @@
     var heatingHtml = '';
     for (var h = 1; h <= Math.max(1, counts.heating); h++) {
       var hs = counts.heating > 1 ? ' #' + h : '';
-      heatingHtml += cmdBlock('Mode chauffage' + hs, 'heating_mode_cmd_' + h, p['heating_mode_cmd_' + h]);
+      heatingHtml += cmdBlock('Mode chaud' + hs, 'heating_mode_hot_cmd_' + h, p['heating_mode_hot_cmd_' + h]);
+      heatingHtml += cmdBlock('Mode froid' + hs, 'heating_mode_cold_cmd_' + h, p['heating_mode_cold_cmd_' + h]);
       heatingHtml += cmdBlock('Chauffage ON' + hs, 'heating_on_cmd_' + h, p['heating_on_cmd_' + h]);
       heatingHtml += cmdBlock('Chauffage OFF' + hs, 'heating_off_cmd_' + h, p['heating_off_cmd_' + h]);
       heatingHtml += cmdBlock('Consigne chaud' + hs, 'setpoint_hot_cmd_' + h, p['setpoint_hot_cmd_' + h]);
